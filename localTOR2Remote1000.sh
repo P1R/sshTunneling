0fa3d3d9236d76b3bc82a3c04139cd5bac613e0d
p1r0 <p1r0@insomnia247.nl>
2018-09-03 12:49:15 -0500

    adding some tor script and fixing some other things

---
+++ localTOR2Remote1000.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#this script clone local orbot from the celphone to my laptop's conected as AP on port 10000, so that i can jump some limits of the ISP cellphone provider.
+ssh -p 64245 -gnNCTR  0.0.0.0:10000:127.0.0.1:9050 p1r0@192.168.43.166
